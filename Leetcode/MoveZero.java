02324e3d30e2f1c5acc4f3952985f5d6d73a5942
Surya.V <krishnasurya2011@gmail.com>
2025-12-16 13:51:52 +0530

    update

---
+++ Leetcode/MoveZero.java
@@ -0,0 +1,16 @@
+class MoveZero {
+    public void moveZeroes(int[] nums) {
+        int insertPos = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[insertPos] = nums[i];
+                insertPos++;
+            }
+        }
+        
+        while (insertPos < nums.length) {
+            nums[insertPos] = 0;
+            insertPos++;
+        }
+    }
+}
---
+++ Leetcode/Rotate.java
@@ -0,0 +1,24 @@
+class Rotate {
+
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k %= n; // Ensure k is within array bounds
+
+        // Reverse the entire array
+        reverse(nums, 0, n - 1);
+        // Reverse the first k elements
+        reverse(nums, 0, k - 1);
+        // Reverse the remaining n-k elements
+        reverse(nums, k, n - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
