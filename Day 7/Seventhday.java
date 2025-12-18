c3dbae81eae079140bd9b39501af0af90fbcf7af
Surya.V <krishnasurya2011@gmail.com>
2025-12-18 14:09:17 +0530

    update

---
+++ Day 7/Seventhday.java
@@ -0,0 +1,28 @@
+// public class Seventhday {
+//     class and object
+//      class
+//      class is a template ot blueprint
+//       class description
+//       class MyClass{
+//     filed,constructor,and method decleration
+//     }
+       
+    //    objects
+    // object is a real time entity
+    //  object occupy size
+
+    // Variables
+    
+    // methods
+
+    // method decleration
+    // types 1. Predefined 2. user defined method
+
+    // Access Modifier
+
+    // constructor
+    // polymorphism
+    // types compile time -static
+    // run time-dynamic
+
+// }
