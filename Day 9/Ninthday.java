aa413ec61b6cc9c6849e3eb8f5884af7a8f85963
Surya.V <krishnasurya2011@gmail.com>
2025-12-20 11:29:58 +0530

    update

---
+++ Day 9/Ninthday.java
@@ -0,0 +1,10 @@
+// public class Ninthday {
+//     Exception
+//     Exception handling
+//     Error
+//     key words
+//     try
+//     catch
+//     throw
+//     finaly
+// }
---
+++ Leetcode/RotateString.java
@@ -0,0 +1,8 @@
+class RotateString {
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        return (s + s).contains(goal);
+    }
+}
